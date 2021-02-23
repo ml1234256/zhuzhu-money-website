61c9098aea04225ed8abe1c984cd3c1c3932d40f
twodays <1064575122@qq.com>
2021-02-23 16:34:50 +0800

    deploy

---
+++ precache-manifest.51d187d3051e0d5aaf3e1f769344b081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f10cd284f83cf29a814",
+    "url": "/zhuzhu-money-website/css/app.d9302bab.css"
+  },
+  {
+    "revision": "199b1b720b49e57a92d2dd4cb2b4ce9a",
+    "url": "/zhuzhu-money-website/index.html"
+  },
+  {
+    "revision": "7f10cd284f83cf29a814",
+    "url": "/zhuzhu-money-website/js/app.8efbfc80.js"
+  },
+  {
+    "revision": "fb2bc185282862810dfa",
+    "url": "/zhuzhu-money-website/js/chunk-vendors.569da67a.js"
+  },
+  {
+    "revision": "e3a781992b68212c0f3ceb8977fb7e10",
+    "url": "/zhuzhu-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/zhuzhu-money-website/robots.txt"
+  }
+]);
